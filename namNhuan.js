8b6baa242dcb98a26a80ec6f713224b3ae224c06
phuc394 <phuc.thg02394@sinhvien.hoasen.edu.vn>
2025-08-01 16:48:41 +0700

    Add Thangtrongngay fuction

--- namNhuan.js
+++ namNhuan.js
@@ -1,9 +1,10 @@
 function NamNhuan (year) {
+
     if (year % 100 === 0) {
         if (year % 400 === 0) {
             return `${year} la nam nhuan`;
         } else {
-            return `${year} khong la nam nhuan`;
+            return `${year} la khong nam nhuan`;
         }
     } else {
         if (year % 4 === 0) {
@@ -14,5 +15,21 @@ function NamNhuan (year) {
     }
 }
 
+function Thangtrongngay(month, year) {
+    if (month < 1 || month > 12) {
+        return "Thang khong hop le";
+    }
+
+    const daysInMonth = [31, (NamNhuan(year).includes("khong la nam nhuan") ? 28 : 29), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    return `Thang ${month} nam ${year} co ${daysInMonth[month - 1]} ngay.`;
+}
+
+
+
+
+
 let year = parseInt(prompt("Nhap nam: "));
 console.log(NamNhuan(year));
+
+let month = parseInt(prompt("Nhap thang: "));
+console.log(Thangtrongngay(month, year));
